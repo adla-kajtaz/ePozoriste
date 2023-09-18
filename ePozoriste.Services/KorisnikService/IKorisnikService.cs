e06319dcf4a36302b985bbf1ae1191ff73f16d5e
adla-kajtaz <adla.kajtaz@edu.fit.ba>
2023-09-18 12:09:42 +0200

    Deleted unsued lines Modified korisnik model

--- ePozoriste.Services/KorisnikService/IKorisnikService.cs
+++ ePozoriste.Services/KorisnikService/IKorisnikService.cs
@@ -12,7 +12,6 @@ namespace ePozoriste.Services
     public interface IKorisnikService : IBaseCRUDService<Model.Korisnik, BaseSearchObject, KorisnikInsertRequest, KorisnikInsertRequest>
     {
         Task<Model.Korisnik> Login(string korisnickoIme, string lozinka);
-        Task<Model.Korisnik> LoginAdmin(string korisnickoIme, string lozinka)
         Model.Korisnik GetByUsername(string korisnickoIme);
     }
 }
